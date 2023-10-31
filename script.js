82a25fa84ca15b6fe5f02b2f240748e078714e7c
Brayden-g-21 <brayden.jgibson15@gmail.com>
2023-10-31 12:31:37 -0600

    completed practice and crash course walkthrough

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
+//Main JavaScript Code
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('#msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields'
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+        
+        userList.appendChild(li);
+
+        //Clear Fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }   
+}
 
 
+
+
+
+
+/*const btn = document.querySelector('btn');
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background = "#ccc";
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+})
+
+/*const ul = document.querySelector('.items');
+ul.remove();
+ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+btn.style.background = 'red';*/
 
 // Console
 /*console.log('Hello World');
